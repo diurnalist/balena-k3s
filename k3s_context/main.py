92a512d77face396b3b26f758e89434f252728a6
Jason <diurnalist@gmail.com>
2021-12-12 16:32:06 -0600

    refactor to use bootstrap token flow
    the device api key given to the devices doesn't have enough scopes
    to read or write env vars on other devices, the operator will have
    to provide a Balena API key themselves to the k3s_context service
    on whichever device they want to be the server node.

--- k3s_context/main.py
+++ k3s_context/main.py
@@ -14,20 +14,12 @@ logging.basicConfig(
 LOG = logging.getLogger("k3s_context")
 
 fleet_id = os.getenv("BALENA_APP_ID")
-fleet_name = os.getenv("BALENA_APP_NAME")
 device_uuid = os.getenv("BALENA_DEVICE_UUID")
-api_token = os.getenv("BALENA_API_KEY")
-k3s_role = os.getenv("K3S_ROLE")
 
-LOG.info(f"balena fleet_id={fleet_id}, fleet_name={fleet_name}, device={device_uuid}")
+token_env_name = "BOOTSTRAP_API_KEY"
+api_token = os.getenv(token_env_name)
 
-if fleet_id == "1":
-    LOG.error("contextualization is not possible in local dev mode.")
-    sys.exit(1)
-
-if not api_token:
-    LOG.error("no api token detected, is the balena-api label on the container?")
-    sys.exit(1)
+LOG.info(f"balena fleet_id={fleet_id}")
 
 balena_api_root = "https://api.balena-cloud.com/v6"
 
@@ -41,7 +33,9 @@ def balena_get(path):
         },
     )
     res.raise_for_status()
-    return res.json()
+    json = res.json()
+    # Unwrap lists automatically
+    return json["d"] if "d" in json else json
 
 
 def balena_set_device_var(device_id, name, value):
@@ -113,64 +107,47 @@ def balena_set_fleet_var(name, value):
 
 
 LOG.info("entering wait loop")
-loop_interval = 5.0  # seconds
-while True:
-    if not k3s_role:
-        LOG.info("no k3s role detected, checking if cluster is bootstrapped")
-        fleet_devices = balena_get(
-            f"/device?$filter=belongs_to__application eq {fleet_id}"
+default_interval = 3600.0  # seconds
+
+
+def process_loop():
+    if not api_token:
+        LOG.info("no bootstrap token detected, idling...")
+        return
+
+    if fleet_id == "1":
+        LOG.error("contextualization is not supported in local dev mode.")
+        return
+
+    LOG.info("checking if bootstrap can continue")
+    service_vars = balena_get(
+        "/device_service_environment_variable?$filter="
+        f"service_install/device eq {device_id} and name eq '{token_env_name}'"
+    )
+    if len(service_vars) != 1:
+        LOG.error(
+            "there should be exactly 1 device with a service environment variable "
+            f"{token_env_name} set for the k3s_context service."
         )
-        devices_with_server_role = [
-            var["device"]
-            for var in balena_get(
-                (
-                    "/device_environment_variable?$filter="
-                    f"device in ({','.join(d['id'] for d in fleet_devices)})"
-                )
-            )
-            if var["name"] == "K3S_ROLE" and var["value"] == "server"
-        ]
+        return
 
-        if not devices_with_server_role:
-            # Pick the oldest online device to be the server.
-            server_device = next(
-                sorted(
-                    (d for d in fleet_devices if d["is_online"] == True),
-                    key=itemgetter("id"),
-                ),
-                None,
-            )
-            assert server_device is not None
-            balena_set_device_var(server_device["id"], "K3S_ROLE", "server")
-        else:
-            # assume somehow the fleet var got deleted; we know there is
-            # a server and it _should_ have set this var, but maybe somebody deleted it.
-            balena_set_fleet_var("K3S_ROLE", "agent")
-
-        break
-
-    LOG.info(f"k3s_role={k3s_role}")
-
-    if k3s_role == "server":
-        device = balena_get(f"/device?$filter=uuid eq '{device_uuid}'")
-        ip_addr = next(iter(device["ip_address"].split()), None)
-        assert ip_addr is not None
-        balena_set_fleet_var("K3S_URL", f"https://{ip_addr}:6443")
-        balena_set_fleet_var("K3S_ROLE", "agent")
-        # If we are the server, monitor the node token and ensure that fleet variables
-        # are properly set.
-        try:
-            with open("/var/lib/rancher/k3s/server/node-token", "r") as f:
-                balena_set_fleet_var("K3S_TOKEN", f.read())
-        except OSError:
-            LOG.info("waiting for node enroll token to appear")
-            loop_interval = 5.0
-        else:
-            # if we got here, we completed most setup successfully, slow down the
-            # processing loop now.
-            loop_interval = 60.0
+    device = balena_get(f"/device?$filter=uuid eq '{device_uuid}'")
+    balena_set_device_var(server_device["id"], "K3S_ROLE", "server")
+    balena_set_fleet_var("K3S_ROLE", "agent")
+    device_ip = device["id"]["ip_address"].split()[0]
+    balena_set_fleet_var("K3S_URL", f"https://{device_ip}:6443")
+    try:
+        with open("/var/lib/rancher/k3s/server/node-token", "r") as f:
+            balena_set_fleet_var("K3S_TOKEN", f.read().strip())
+    except OSError:
+        LOG.info("waiting for node enroll token to appear")
+        return 5.0
     else:
-        # we are an agent, not much for us to do, set a longer interval
-        loop_interval = 60.0
+        # if we got here, we completed most setup successfully, slow down the
+        # processing loop now.
+        return 60.0
 
-    time.sleep(loop_interval)
+
+while True:
+    next_interval = process_loop()
+    time.sleep(next_interval or default_interval)
